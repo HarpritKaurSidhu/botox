7b77bd8fbcf87db33ff9c372fa56b4703d4f8335
Manoj <manoj.itmatic@gmail.com>
2017-05-15 18:22:37 +0530

    take photograph functionality is done

--- app/src/main/java/io/itmatic/botox/Patient/TakePhotograph.java
+++ app/src/main/java/io/itmatic/botox/Patient/TakePhotograph.java
@@ -10,11 +10,13 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,11 +31,16 @@ public class TakePhotograph extends BaseActivity implements SurfaceHolder.Callba
     SurfaceView surface_camera;
     @BindView(R.id.btn_camera)
     Button buttonCamera;
+    @BindView(R.id.img_camera_front)
+    ImageView imageViewFrontCamera;
+    @BindView(R.id.img_camera_rear)
+    ImageView imageViewRearCamera;
     Camera camera;
     SurfaceHolder surfaceHolder;
     Camera.PictureCallback rawCallback;
     Camera.ShutterCallback shutterCallback;
     Camera.PictureCallback jpegCallback;
+    int position=1;
 
 
 
@@ -49,12 +56,175 @@ public class TakePhotograph extends BaseActivity implements SurfaceHolder.Callba
         getSupportActionBar().setTitle(getResources().getString(R.string.confirm_booking));
 
 
+        setCameraOnSurface();
+
+        imageViewFrontCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                position=1;
+               camera.stopPreview();
+                camera.release();
+                camera = Camera.open(position);
+                camera.setDisplayOrientation(90);
+                try {
+                    camera.setPreviewDisplay(surfaceHolder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                camera.startPreview();
+
+
+            }
+        });
+
+        imageViewRearCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                position=0;
+                camera.stopPreview();
+                camera.release();
+                camera = Camera.open(position);
+                camera.setDisplayOrientation(90);
+                try {
+                    camera.setPreviewDisplay(surfaceHolder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                camera.startPreview();
+
+            }
+        });
+
+
         buttonCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 surfaceCreated(surfaceHolder);
             }
         });
+
+
+    }
+    public void captureImage(View v) throws IOException {
+        //take the picture
+
+        camera.takePicture(null, null, jpegCallback);
+    }
+
+    public void refreshCamera() {
+        if (surfaceHolder.getSurface() == null) {
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            camera.stopPreview();
+
+        } catch (Exception e) {
+
+            // ignore: tried to stop a non-existent preview
+        }
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+        // start preview with new settings
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+            camera.setDisplayOrientation(90);
+        } catch (Exception e) {
+
+        }
+
+    }
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // Now that the size is known, set up the camera parameters and begin
+        // the preview.
+        refreshCamera();
+
+    }
+
+
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            // open the camera
+            camera = Camera.open(position);
+        } catch (RuntimeException e) {
+            // check for exceptions
+            System.err.println(e);
+            return;
+        }
+        Camera.Parameters param;
+        param = camera.getParameters();
+        // modify parameter
+
+        List<Camera.Size> sizes = param.getSupportedPreviewSizes();
+        Camera.Size optimalSize = getOptimalPreviewSize(sizes, getResources().getDisplayMetrics().widthPixels, getResources().getDisplayMetrics().heightPixels);
+        param.setPreviewSize(optimalSize.width, optimalSize.height);
+        camera.setParameters(param);
+        try {
+            // The Surface has been created, now tell the camera where to draw
+            // the preview.
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (Exception e) {
+            // check for exceptions
+            System.err.println(e);
+            return;
+        }
+
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // stop preview and release camera
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+
+    }
+
+
+
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.05;
+        double targetRatio = (double) w/h;
+
+        if (sizes==null) return null;
+
+        Camera.Size optimalSize = null;
+
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        // Find size
+        for (Camera.Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+
+
+    private void setCameraOnSurface()
+    {
         surfaceHolder = surface_camera.getHolder();
         // Install a SurfaceHolder.Callback so we get notified when the
         // underlying surface is created and destroyed
@@ -85,92 +255,13 @@ public class TakePhotograph extends BaseActivity implements SurfaceHolder.Callba
 
                 }
 
-               // Toast.makeText(getApplicationContext(), "Picture Saved", 2000).show();
+                // Toast.makeText(getApplicationContext(), "Picture Saved", 2000).show();
 
                 refreshCamera();
 
             }
         };
-
     }
-    public void captureImage(View v) throws IOException {
-        //take the picture
-
-        camera.takePicture(null, null, jpegCallback);
-    }
-
-    public void refreshCamera() {
-        if (surfaceHolder.getSurface() == null) {
-            // preview surface does not exist
-            return;
-        }
-
-        // stop preview before making changes
-        try {
-            camera.stopPreview();
-
-        } catch (Exception e) {
-
-            // ignore: tried to stop a non-existent preview
-        }
-        // set preview size and make any resize, rotate or
-        // reformatting changes here
-        // start preview with new settings
-        try {
-            camera.setPreviewDisplay(surfaceHolder);
-            camera.startPreview();
-        } catch (Exception e) {
-
-        }
-
-    }
-    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-        // Now that the size is known, set up the camera parameters and begin
-        // the preview.
-        refreshCamera();
-
-    }
-
-
-
-    public void surfaceCreated(SurfaceHolder holder) {
-        try {
-            // open the camera
-            camera = Camera.open();
-        } catch (RuntimeException e) {
-            // check for exceptions
-            System.err.println(e);
-            return;
-        }
-        Camera.Parameters param;
-        param = camera.getParameters();
-        // modify parameter
-        param.setPreviewSize(352, 288);
-        camera.setParameters(param);
-        try {
-            // The Surface has been created, now tell the camera where to draw
-            // the preview.
-            camera.setPreviewDisplay(surfaceHolder);
-            camera.startPreview();
-        } catch (Exception e) {
-            // check for exceptions
-            System.err.println(e);
-            return;
-        }
-
-    }
-
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        // stop preview and release camera
-        camera.stopPreview();
-        camera.release();
-        camera = null;
-
-    }
-
-
-
-
 
 
 }
